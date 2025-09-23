3c023da9f427a6628f74c71f9c72f1e61fa41325
Hamim Kakar <hamimkakarr@gmail.com>
2025-09-23 14:18:21 +0100

    fixed the hyperlink for series on index.html

--- app_organized.py
+++ app_organized.py
@@ -53,6 +53,11 @@ def topics_list():
 def series_basics():
     return render_template("SeriesCP1/SeriesBasics.html")
 
+@app.route("/SeriesFrontPage")
+def series_front_page():
+    return render_template("SeriesCP1/SeriesFrontPage.html")
+
+
 # ============================================
 # TOPIC OVERVIEW PAGES (components/ folder)
 # ============================================
